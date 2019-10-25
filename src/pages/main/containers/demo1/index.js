9197bf529687e9186d5932e05e0e6985d27ddf0f
Huge_Ming <LancelotM@users.noreply.github.com>
2019-10-25 12:02:14 +0800

    #Change从一个组件获取另一个组件值的代码

--- src/pages/main/containers/demo1/index.js
+++ src/pages/main/containers/demo1/index.js
@@ -5,7 +5,9 @@ import { bindActionCreators } from 'redux';
 import * as actions from './action.js';
 let num = actions.num;
 import * as actions2 from '../demo2/action.js';
+let num2 = actions2.num;
 import * as actions3 from '../demo3/action.js';
+let num3 = actions3.num;
 
 class Demo1 extends React.Component {
 	constructor(props) {
@@ -27,21 +29,27 @@ class Demo1 extends React.Component {
     	actions3.clearNum();
     }
 	render() {
-		const {actions, json} = this.props;
-		console.log('demo1 render');
-		console.log(json);
+		const {actions, demo1, demo2 ,demo3} = this.props;
 		return(
 			<div>
-				<h1 onClick={()=>{actions.addNum(json[num])}}>我是demo1 我被点了 {json[num]} 次</h1>
+				<h1 onClick={()=>{actions.addNum(demo1.json[num])}}>我是demo1 我被点了 {demo1.json[num]} 次</h1>
 				<button onClick={()=>{this.clearNum()}}>归零自己</button>
+				<h1>demo2 被点了 {demo2.json[num2]} 次</h1>
 				<button onClick={()=>{this.clearNum2()}}>归零demo2</button>
+				<h1>demo3 被点了 {demo3.json[num3]} 次</h1>
 				<button onClick={()=>{this.clearNum3()}}>归零demo3</button>
 			</div>
 		)
 	}
 }
 export default connect(
-    state=>Object.assign(state.Demo1),
+    state=>{
+			return {
+				demo1:state.Demo1,
+				demo2:state.Demo2,
+				demo3:state.Demo3,
+			}
+		},
     dispatch=>({
 		actions:bindActionCreators(actions,dispatch),
 		actions2:bindActionCreators(actions2,dispatch),
